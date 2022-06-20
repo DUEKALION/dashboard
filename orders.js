b29fa350ca105c78cf81dd5397b616e6e843dc6b
Oni-olufemi Ayobami <aycodergymer21@gmail.com>
2022-06-20 15:12:47 +0100

    5th commit added an order.js file

---
+++ orders.js
@@ -0,0 +1,36 @@
+const orders =  [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+
+    {
+        productName: 'Ruko Fill Pro Drone',
+        productNumber: '48924',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '22353',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    }
+]
